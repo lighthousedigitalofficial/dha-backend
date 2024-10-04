e42babcb2af924516f713a14f079a68ff6e2f993
naimatofficial <naimat.official.10@gmail.com>
2024-10-04 17:40:11 +0500

    update routes and validation schemas

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,23 +1,25 @@
 import express from "express";
 import { validate } from "../middleware/validationMiddleware.js";
-import { videoValidationSchema } from "../validationSchema/videoValidation.js";
+import { videoValidationSchema } from "../validations/videoValidation.js";
 import {
-    createVideo,
-    getVideos,
-    getVideo,
-    deleteVideo,
-    updateVideo,
+	createVideo,
+	getVideos,
+	getVideo,
+	deleteVideo,
+	updateVideo,
 } from "../controllers/videoController.js";
 
 const router = express.Router();
 
-router.route("/")
-    .get(getVideos)
-    .post(validate(videoValidationSchema), createVideo); 
+router
+	.route("/")
+	.get(getVideos)
+	.post(validate(videoValidationSchema), createVideo);
 
-router.route("/:id")
-    .get(getVideo)
-    .put(validate(videoValidationSchema), updateVideo) 
-    .delete(deleteVideo);
+router
+	.route("/:id")
+	.get(getVideo)
+	.put(validate(videoValidationSchema), updateVideo)
+	.delete(deleteVideo);
 
 export default router;
