5c6decfb60f2e9174861aa96f02a093897c26697
Wasim <princewasim971@gmail.com>
2024-10-04 22:38:58 -0700

    add validation

--- routes/registrationPropertyRoutes.js
+++ routes/registrationPropertyRoutes.js
@@ -1,19 +1,25 @@
 import express from "express";
 import {
-    createRegistrationProperty,
-    deleteRegistrationProperty,
-    getRegistrationProperty,
-    getRegistrationProperties,
-    updateRegistrationProperty,
+  createRegistrationProperty,
+  deleteRegistrationProperty,
+  getRegistrationProperty,
+  getRegistrationProperties,
+  updateRegistrationProperty,
 } from "../controllers/registrationPropertyController.js";
+import { validateSchema } from "../middleware/validateSchema.js";
+import { registrationPropertyValidation }
+ from "../validations/registrationPropertyValidation.js";
 
 const router = express.Router();
 
-router.route("/").post(createRegistrationProperty).get(getRegistrationProperties);
-router
-    .route("/:id")
-    .get(getRegistrationProperty)
-    .put(updateRegistrationProperty)
-    .delete(deleteRegistrationProperty);
+// Routes for Registration Properties
+router.route("/")
+  .post(validateSchema(registrationPropertyValidation), createRegistrationProperty)
+  .get(getRegistrationProperties);
+
+router.route("/:id")
+  .get(getRegistrationProperty)
+  .put(validateSchema(registrationPropertyValidation), updateRegistrationProperty)
+  .delete(deleteRegistrationProperty);
 
 export default router;
--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -1,19 +1,24 @@
 import express from "express";
 import {
-    createTeam,
-    deleteTeam,
-    getTeam,
-    getTeams,
-    updateTeam,
+  createTeam,
+  deleteTeam,
+  getTeam,
+  getTeams,
+  updateTeam,
 } from "../controllers/teamController.js";
+import { validateSchema } from "../middleware/validateSchema.js";
+import { teamValidation } from "../validations/teamValidation.js";
 
 const router = express.Router();
 
-router.route("/").post(createTeam).get(getTeams);
-router
-    .route("/:id")
-    .get(getTeam)
-    .put(updateTeam)
-    .delete(deleteTeam);
+// Routes for Team Management
+router.route("/")
+  .post(validateSchema(teamValidation), createTeam)
+  .get(getTeams);
+
+router.route("/:id")
+  .get(getTeam)
+  .put(validateSchema(teamValidation), updateTeam)
+  .delete(deleteTeam);
 
 export default router;
