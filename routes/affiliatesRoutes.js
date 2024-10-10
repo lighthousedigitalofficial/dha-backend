205f402d32796546da13ac787f41ad4c390915b6
naimatofficial <naimat.official.10@gmail.com>
2024-10-10 15:36:50 +0500

    add prettierrc

--- routes/affiliatesRoutes.js
+++ routes/affiliatesRoutes.js
@@ -1,26 +1,25 @@
-import express from "express";
+import express from 'express'
 
-import checkObjectId from "../middleware/checkObjectId.js";
+import checkObjectId from '../middleware/checkObjectId.js'
 import {
-  createAffiliates,
-  deleteAffiliates,
-  getAffiliates,
-  getAllAffiliates,
-  getBySlug,
-  updateAffiliates,
-} from "../controllers/affiliatesController.js";
+    createAffiliates,
+    deleteAffiliates,
+    getAffiliates,
+    getAllAffiliates,
+    getBySlug,
+    updateAffiliates,
+} from '../controllers/affiliatesController.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.route("/").post(createAffiliates).get(getAllAffiliates);
+router.route('/').post(createAffiliates).get(getAllAffiliates)
 
-router.route("/slug/:slug").get(getBySlug);
-
+router.route('/slug/:slug').get(getBySlug)
 
 router
-  .route("/:id", checkObjectId)
-  .get(getAffiliates)
-  .put(updateAffiliates)
-  .delete(deleteAffiliates);
+    .route('/:id', checkObjectId)
+    .get(getAffiliates)
+    .put(updateAffiliates)
+    .delete(deleteAffiliates)
 
-export default router;
+export default router
--- routes/engineersRoutes.js
+++ routes/engineersRoutes.js
@@ -1,24 +1,24 @@
-import express from "express";
+import express from 'express'
 
-import checkObjectId from "../middleware/checkObjectId.js";
+import checkObjectId from '../middleware/checkObjectId.js'
 import {
-  createEngineers,
-  deleteEngineers,
-  getAllEngineers,
-  getBySlug,
-  getEngineers,
-  updateEngineers,
-} from "../controllers/engineersController.js";
+    createEngineers,
+    deleteEngineers,
+    getAllEngineers,
+    getBySlug,
+    getEngineers,
+    updateEngineers,
+} from '../controllers/engineersController.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.route("/").post(createEngineers).get(getAllEngineers);
-router.route("/slug/:slug").get(getBySlug);
+router.route('/').post(createEngineers).get(getAllEngineers)
+router.route('/slug/:slug').get(getBySlug)
 
 router
-  .route("/:id", checkObjectId)
-  .get(getEngineers)
-  .put(updateEngineers)
-  .delete(deleteEngineers);
+    .route('/:id', checkObjectId)
+    .get(getEngineers)
+    .put(updateEngineers)
+    .delete(deleteEngineers)
 
-export default router;
+export default router
